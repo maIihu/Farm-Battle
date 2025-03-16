9237dc5efe65969c0603a1510f012d1dcbe11554
Nguyen Manh Hung <nguyenmanhhung20184@gmail.com>
2025-03-16 17:40:14 +0700

    Update Shop

--- Assets/_Scripts/UIShop.cs
+++ Assets/_Scripts/UIShop.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,24 +11,41 @@ public class UIShop : MonoBehaviour
     [SerializeField] private GameObject shopItemTemplate;
     private List<GameObject> _items;
     private int _currentIndex;
+    private int _itemCount;
     private void Start()
     {
         shop1.SetActive(false);
         _items = new List<GameObject>();
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                CreateItemShop(i, j);
-            }
-        }
+        CreateShop();
+        _itemCount = _items.Count;
     }
 
-    private void CreateItemShop(int x, int y)
+    private void CreateShop()
+    {
+        // for (int i = 0; i < 2; i++)
+        // {
+        //     for (int j = 0; j < 3; j++)
+        //     {
+        //         CreateItemShop(i, j);
+        //     }
+        // }
+        CreateItemShop(Item.GetDescribe(Item.ItemType.Shield),0, 0);
+        CreateItemShop(Item.GetDescribe(Item.ItemType.Rain),0, 1);
+        CreateItemShop(Item.GetDescribe(Item.ItemType.Thunder),0, 2);
+        CreateItemShop(Item.GetDescribe(Item.ItemType.Wave),1, 0);
+        CreateItemShop(Item.GetDescribe(Item.ItemType.Wind),1, 1);
+        CreateItemShop("Hello1",1, 2);
+        
+    }
+    
+    private void CreateItemShop(string text, int x, int y)
     {
         GameObject itemClone = Instantiate(shopItemTemplate, shop1.transform);
+        itemClone.name = text;
         RectTransform rectTransform = itemClone.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = new Vector2(-40 + 80 * x, 110 - 80 * y);
+        itemClone.GetComponentInChildren<TextMeshProUGUI>().text = text;
+        itemClone.transform.GetChild(1).gameObject.SetActive(false);
         
         _items.Add(itemClone);
     }
@@ -36,25 +54,45 @@ public class UIShop : MonoBehaviour
     {
         if (shop1.activeSelf)
         {
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.UpArrow)) MoveSelection(-1); 
+            if (Input.GetKeyDown(KeyCode.DownArrow)) MoveSelection(1);  
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveSelection(-3); 
+            if (Input.GetKeyDown(KeyCode.RightArrow)) MoveSelection(3);
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                MoveSelection(1);
+                Debug.Log(_items[_currentIndex]);
             }
+            
         }
     }
 
     private void MoveSelection(int direction)
     {
-        int newIndex = _currentIndex + direction;
-        
+        int row = _currentIndex % 3; // 0 1 2
+        int col = _currentIndex / 3; // 0 1
 
-            HighlightItem(newIndex);
+        int newRow = row;
+        int newCol = col;
+
+        if (direction == -1 && row > 0) newRow--;
+        if (direction == 1 && row < 2) newRow++;
+        if (direction == -3 && col > 0) newCol--;
+        if (direction == 3 && col < 1) newCol++;
+
+        int newIndex = newCol * 3 + newRow;
         
+        HighlightItem(newIndex);
+        _currentIndex = newIndex;
     }
 
     private void HighlightItem(int index)
     {
+        _items[_currentIndex].GetComponentInChildren<Image>().color = Color.white;
+        _items[_currentIndex].transform.GetChild(1).gameObject.SetActive(false);
+        
         _items[index].GetComponentInChildren<Image>().color = Color.yellow;
+        _items[index].transform.GetChild(1).gameObject.SetActive(true);
     }
     
     private void OnCollisionEnter2D(Collision2D other)
@@ -70,6 +108,12 @@ public class UIShop : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            foreach (var item in _items)
+            {
+                item.GetComponentInChildren<Image>().color = Color.white;
+                item.transform.GetChild(1).gameObject.SetActive(false);
+            }
+            // The object of type 'GameObject' has been destroyed but you are still trying to access it.
             shop1.SetActive(false);
         }
     }
